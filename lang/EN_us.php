2418471d62752d0455258086df9a4e081a848829
Räganr O'ock <38219333+UgoDumontot@users.noreply.github.com>
2019-09-09 20:21:17 +0200

    update lang/en

--- lang/EN_us.php
+++ lang/EN_us.php
@@ -1,6 +1,8 @@
 <?php
   define('AUTO_REFRESH', 'Auto refresh');
   define('DARK_MODE', 'Dark mode');
+  define('STREAM_MODE', 'Streaming mode');
+  define('SHOW_ID', 'Show IDs');
   define('REFRESH_BUTTON', 'Refresh the list');
   define('NOSCRIPT', 'Please turn JavaScript on');
   define("PLAYLIST_LENGTH_DISPLAY_LABEL_PRE", "");
@@ -9,4 +11,5 @@
   define("PLAYLIST_LENGTH_TOTAL_PRE", "there is ");
   define("PLAYLIST_LENGTH_TOTAL_POST", " music(s) in the playlist");
   define("FOOTER_AUTOR", "Made with ❤️, by ");
+  define("FOOTER_ISSUE", "Une erreure ou un bug? Rapporte le sur <a href='https://discord.gg/warths'>Discord</a> ou sur <a href='https://github.com/Ragnar-Oock/HeavyProjectMusic/issues'>Github</a>");
 ?>
