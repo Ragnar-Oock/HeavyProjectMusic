d2ff543e3155ff84ee25029b48a9bb875416a882
Räganr O'ock <38219333+UgoDumontot@users.noreply.github.com>
2019-09-12 21:00:30 +0200

    correct spelling for french footer

--- lang/FR_fr.php
+++ lang/FR_fr.php
@@ -11,5 +11,5 @@
   define("PLAYLIST_LENGTH_TOTAL_PRE", "il y a ");
   define("PLAYLIST_LENGTH_TOTAL_POST", " musique(s) dans la playlist");
   define("FOOTER_AUTOR", "Fait avec ❤️, par ");
-  define("FOOTER_ISSUE", "Une erreure ou un bug? Rapporte le sur <a href='https://github.com/Ragnar-Oock/HeavyProjectMusic/issues'>Github</a>");
+  define("FOOTER_ISSUE", "Une erreur ou un bug? Rapportes le sur <a href='https://github.com/Ragnar-Oock/HeavyProjectMusic/issues'>Github</a>");
 ?>
