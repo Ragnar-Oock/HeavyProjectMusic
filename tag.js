a4a8b0c22b564843da18477333ef6e8c843fbc8f
Räganr O'ock <38219333+UgoDumontot@users.noreply.github.com>
2019-10-16 15:57:07 +0200

    🛠 add tag auto update and tag auto move

--- tag.js
+++ tag.js
@@ -1,7 +1,10 @@
 class Tag {
-  constructor(obj) {
+  constructor(obj, parentId, index) {
+    this.id = obj.id;
+    this.parentId = parentId;
+    this.index = index;
     this.type = typeof obj.color !== 'undefined' ? obj.type : 'text';
-    this.time = typeof this.type == 'time' && obj.time !== 'undefined' ? obj.time : Date.now();
+    this.time = typeof this.type == 'time' && obj.time !== 'undefined' ? obj.time : undefined;
     this.text = obj.text;
     this.icon = typeof obj.icon !== 'undefined' ? obj.icon : '';
     this.ariaLabel = typeof obj.ariaLabel !== 'undefined' ? obj.ariaLabel : '';
@@ -9,30 +12,53 @@ class Tag {
     this.fontColor = typeof obj.fontColor !== 'undefined' ? obj.fontColor : 'fff';
 
     this.lastTimer = this.getTimer();
-    return this.toHtml();
+
+    if (this.type === 'timer') {
+      this.timer = setInterval(() => {
+        $('#tag' + this.parentId + '-' + this.id).replace(this.toHtml())
+      }, 60000);
+    }
+
+    return this.html;
   }
 
+  /**
+   * @returns formated HTML for the tag
+   */
   toHtml() {
     let text = this.text.replace('%TIME%', this.lastTimer),
       ariaLabel = this.ariaLabel.replace('%TIME%', this.lastTimer),
       img = this.icon === '' ? `<img class="badge" src="${this.icon}" aria-label="${ariaLabel}">` : '';
 
     return `
-      <figure class="playlist_item__tag" style="background:#${this.color};color:#${this.fontColor}">
+      <figure id="${'tag' + this.parentId + '-' + this.id}" class="playlist_item__tag" style="background:#${this.color};color:#${this.fontColor}">
         ${img}
         <figcaption>${text}</figcaption>
       </figure>`;
   }
 
+  /**
+   * update the tag if needed
+   * @param {object} obj parsed JSON from AJAX request
+   * @returns {boolean} either or not the tag updated itself
+   */
   update(obj) {
     let timer = this.getTimer();
-    if (timer != this.lastTimer || this.isSame(obj)) {
+    if (timer != this.lastTimer || !this.isSameAs(obj)) {
       this.lastTimer = timer;
+      this.html = this.toHtml();
+      $('#tag' + this.parentId + '-' + this.id).replace(this.html);
+      return true;
     }
-    return this.toHtml();
+    return false;
   }
 
-  isSame(obj) {
+  /**
+   * compare the passed obj to this object, updatethe properties as needed and notify it
+   * @param {object} obj parsed JSON from AJAX request
+   * @returns {boolean}  the tag is the same / the tag as bein updated
+   */
+  isSameAs(obj) {
     let same = true;
     if (this.text !== obj.text) {
       this.text = obj.text;
@@ -53,20 +79,46 @@ class Tag {
     return same;
   }
 
+  /**
+   * return the formated timer if this.type = timer, undefine otherwise
+   * @returns {string} formated timer
+   */
   getTimer() {
-    let delta = Math.round((Date.now() - this.time) / 1000),
-      min = Math.round(delta / 60) % 60,
-      hours = Math.round(delta / 3600),
-      time = "error";
-    if (hours === 0) {
-      time = min + 'min';
-    }
-    else if (min === 0) {
-      time = hours + 'h';
+    if (this.type === 'timer') {
+      let delta = Math.round((Date.now() - this.time) / 1000),
+        min = Math.round(delta / 60) % 60,
+        hours = Math.round(delta / 3600),
+        time = "error";
+      if (hours === 0) {
+        time = min + 'min';
+      }
+      else if (min === 0) {
+        time = hours + 'h';
+      }
+      else {
+        time = hours + 'h ' + min + 'min';
+      }
+      return time;
     }
     else {
-      time = hours + 'h ' + min + 'min';
+      return undefined;
     }
-    return time;
+  }
+
+  /**
+   * move the tag to the desired location
+   * @param  {int} index index to move the item to visualy
+   */
+  move(index) {
+    // if the tag move at the first position
+    if (index !== this.index) {
+      if (index === 0) {
+        this.dom.prependTo('#id' + this.parentId + '>.playlist_item__tags');
+      }
+      // if the item move anywhere else
+      else {
+        this.dom.insertAfter('#id' + this.parentId + '>.playlist_item__tags>.playlist_item__tag:nth-child(' + index + ')')
+      }
+    }
   }
 }
